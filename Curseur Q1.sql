3b6fc63595c44a91ab9e94abe64b98f987f4993d
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-03-16 19:28:52 +0100

    Modelisation Q2.3 + Curseur Q1

---
+++ Curseur Q1.sql
@@ -0,0 +1,32 @@
+DECLARE
+    v_ProductRef    NUMBER :=: v_ProductRef;
+    v_Category      VARCHAR2(64);
+    v_Family        VARCHAR2(64);
+    r_Product       cataloge%ROWTYPE;
+    CURSOR c_ProductInfo IS
+        SELECT * FROM cataloge WHERE cataloge.Ref = v_ProductRef;
+    CURSOR c_Category IS
+        SELECT Label INTO v_Category FROM categories WHERE Code = r_Product.Category;
+    CURSOR c_Family IS
+        SELECT Label INTO v_Family FROM families WHERE Code = r_Product.Family;
+
+BEGIN
+    OPEN c_ProductInfo;
+    FETCH c_ProductInfo INTO r_Product;
+    EXIT WHEN c_ProductInfo%NOTFOUND;
+    OPEN  c_Category;
+    FETCH c_Category INTO v_Category;
+    OPEN  c_Family;
+    FETCH c_Family INTO v_Family;
+    DBMS_OUTPUT.PUT_LINE(
+        'Ref: ' || r_Product.Ref ||
+        '- Label: ' || r_Product.Label ||
+        '- Category: ' || v_Category ||
+        '- Family: ' || v_Family ||
+        '- Unite Price: ' || r_Product.UnitePrice ||
+        '- TVA: ' || r_Product.TVA
+        );
+    CLOSE c_ProductInfo;
+    CLOSE c_Category;
+    CLOSE c_Family;
+end;
---
+++ INSERT INTO.sql
@@ -0,0 +1,28 @@
+INSERT INTO clientlist VALUES(1, 'BENTAJER','Mhannech II, Tetouan', 1, '2019-11-02', 'ahmed@mail.com',
+                              'A passé une grosse commande en janvier 2018 ; depuis, plus rien. Penser à reprendre contact');
+INSERT INTO clientlist VALUES(2, 'ESSEGHIRI','Res Naoufal app 8, Maarif, Casablanca', 2, '2018-10-27', 'youssef@mail.com',
+                              'Client contactée grâce à M. Houty de Casablanca. Remise de 10% sur ladernière commande suite à un retard d’envoi');
+INSERT INTO clientlist VALUES(3, 'Belmir', 'Tetouan', 2, '2018-11-20', 'karim@mail.com',
+                              'Client fidèle qui passe des commandes régulièrement (environ une par mois)');
+
+INSERT INTO regions VALUES(1, 'Tanger-Tétouan-Al Hoceïma');
+INSERT INTO regions VALUES(2, 'Région de l Oriental');
+INSERT INTO regions VALUES(3, 'Région de Fès-Meknès');
+INSERT INTO regions VALUES(4, 'Région de Rabat-Salé-Kénitra');
+INSERT INTO regions VALUES(5, 'Région de Béni Mellal-Khénifra');
+INSERT INTO regions VALUES(6, 'Région de Casablanca-Settat');
+INSERT INTO regions VALUES(7, 'Région de Marrakech-Safi');
+
+INSERT INTO categories VALUES(1, 'MEMOIRE');
+INSERT INTO categories VALUES(2, 'CARTE');
+INSERT INTO categories VALUES(3, 'DISQUE');
+
+INSERT INTO catalogue VALUES('A01', 'Barrette Memoire 2Go', 1, 1, 150, '12%');
+INSERT INTO catalogue VALUES('A02', 'Barrette Memoire 4Go', 1, 1, 240, '12%');
+INSERT INTO catalogue VALUES('B15', 'Carte Graphique AMD Radeon RX 570 Pulse, 4Go', 5, 2, 2349, '20%');
+INSERT INTO catalogue VALUES('A11', 'Carte son Asus Xonar DG', 4, 2, 750, '12%');
+INSERT INTO catalogue VALUES('C80', 'Disque Externe Toshiba usb 3.0 1000Go', 2, 3, 546, '33%');
+
+INSERT INTO command VALUES(886, '2019-02-01', 2, 'A01', 10, 0);
+INSERT INTO command VALUES(886, '2019-02-01', 2, 'A02', 2, 0);
+INSERT INTO command VALUES(886, '2019-02-01', 2, 'B15', 1, 0);
