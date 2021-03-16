cda8fad0bbbb9eec68c7e6c579fad3109953e06c
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-03-16 19:57:02 +0100

    Added Function Q4

---
+++ Functions Q4.sql
@@ -0,0 +1,17 @@
+DECLARE
+    v_CategoryID    int :=: v_CatagoryID;
+    v_ArticleCount  int;
+
+    FUNCTION f_CountArticles RETURN NUMBER IS v_TotalArticles NUMBER;
+    BEGIN
+        SELECT COUNT(catalogue.Ref) INTO v_TotalArticles
+        FROM catalogue WHERE catalogue.Category = v_CategoryID;
+
+        RETURN v_TotalArticles;
+    END f_CountArticles;
+BEGIN
+    v_ArticleCount := f_CountArticles();
+
+    DBMS_OUTPUT.PUT_LINE('Lotal Articles of the category ' || v_CategoryID ||
+                         'is: ' || v_ArticleCount);
+end;
