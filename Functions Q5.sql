88082914089449ce87ea6f50fc522246812684e3
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-03-17 18:09:33 +0100

    Added Functions Q5

---
+++ Functions Q5.sql
@@ -0,0 +1,16 @@
+DECLARE
+    v_ClientID      int :=: v_ClientID;
+    v_OrdersCount int;
+
+    FUNCTION f_CountOrders RETURN NUMBER IS v_OrdersCount NUMBER;
+    BEGIN
+        SELECT COUNT(commands.CommandID) INTO v_OrdersCount
+        FROM commands WHERE commands.ClientID = v_ClientID;
+        RETURN v_OrdersCount;
+    END f_CountOrders;
+BEGIN
+    v_OrdersCount := f_CountOrders();
+
+    DBMS_OUTPUT.PUT_LINE('Number of commands for this client is: ' || v_ClientID ||
+                         'is: ' || v_OrdersCount);
+end;
